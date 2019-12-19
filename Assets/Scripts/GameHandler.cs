f1adcfe2627097c51da3c5f64e7c6d6ddd11352b
Emmanuel <emmanuel.roberson3@gmail.com>
2019-12-19 17:58:00 -0600

    Added Game handler
    Started framework for using sprite animations using DOTS

---
+++ Assets/Scripts/GameHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Burst;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Transforms;
+
+public class GameHandler : MonoBehaviour
+{
+
+    public Mesh quadMesh;
+    public Material walkingSpriteSheetMaterial;
+    private void Awake()
+    {
+        EntityManager entityManager = World.Active.EntityManager;
+        var entityArchetype = entityManager.CreateArchetype(typeof(Translation));
+
+        Entity entity = entityManager.CreateEntity(entityArchetype);
+    }
+}
