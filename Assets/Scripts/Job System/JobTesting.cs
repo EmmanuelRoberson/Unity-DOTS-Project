cb653e006ab4ff05182cf24eaf87cf639ca49a73
Emmanuel <emmanuel.roberson3@gmail.com>
2019-12-19 13:01:06 -0600

    Job system Test script

---
+++ Assets/Scripts/Job System/JobTesting.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Jobs;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class JobTesting : MonoBehaviour
+{
+    private void Update()
+    {
+        float startTime = Time.realtimeSinceStartup;
+        
+       JobHandle reallyToughTaskJobHandle =  ReallyToughTaskJob();
+       reallyToughTaskJobHandle.Complete();//pauses the main thread unitl job jhss been completer
+       
+        Debug.Log(((Time.realtimeSinceStartup - startTime) * 1000f) + "ms");
+    }
+
+    private JobHandle ReallyToughTaskJob()
+    {
+        ReallyToughJob job = new ReallyToughJob();
+        
+        //schedule tells the job system to schedule this job to be completed by an available thread when possible
+        //it also returns the job handle which is important to keep track of
+        return job.Schedule();
+
+    }
+}
+
+public struct ReallyToughJob : IJob
+{
+    public void Execute()
+    {
+        //this is meant to represent a tough task, like pathfinding or complex calculation
+        float value = 0f;
+        for (int i = 0; i < 50000; i++)
+        {
+            value = math.exp10(math.sqrt(value));
+        }
+    }
+}
